2cd0e2009e52909518bb7d96ad444bdef65f353b
Nina Nyberg <nina.b.nyberg@gmail.com>
2022-02-08 00:05:36 +0200

    Crazy iteration 5 added

--- js/index.js
+++ js/index.js
@@ -16,8 +16,8 @@ function updateSubtotal(product) {
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
@@ -27,7 +27,7 @@ function calculateAll() {
   const products = document.querySelectorAll('.product');
 
   let sum = 0;
-  products.forEach((item) => (sum += updateSubtotal(item)));
+  if (products) products.forEach((item) => (sum += updateSubtotal(item)));
 
   const total = (document.querySelector('#total-value span').innerHTML = sum);
   // let newArr = [];
@@ -50,7 +50,8 @@ function removeProduct(event) {
   //... your code goes here
   // const removeItem = target.parentNode.parentNode.parentNode;
   // removeItem.removeChild(target.parentNode.parentNode);
-  target.parentNode.parentNode.remove();
+  let itemToRemove = target.parentNode.parentNode;
+  itemToRemove.remove();
   calculateAll();
 }
 
@@ -58,6 +59,43 @@ function removeProduct(event) {
 
 function createProduct() {
   //... your code goes here
+  const lastFieldParent = document.querySelector('.create-product');
+  const nameInput = lastFieldParent.firstElementChild.querySelector('input');
+  let name = nameInput.value;
+
+  const numberInput =
+    lastFieldParent.firstElementChild.nextElementSibling.querySelector('input');
+  let num = numberInput.value;
+  // alert(nameInput + ' ' + numberInput);
+  const selectParent = document.querySelector('tbody');
+  const creatingEl = document.createElement('tr');
+  creatingEl.setAttribute('class', 'product');
+  // alert(1);
+  creatingEl.innerHTML = `<td class="name">
+      <span>${name}</span>
+    </td>
+    <td class="price">$<span>${num}</span></td>
+    <td class="quantity">
+      <input type="number" value="0" min="0" placeholder="Quantity" />
+    </td>
+    <td class="subtotal">$<span>0</span></td>
+    <td class="action">
+      <button class="btn btn-remove">Remove</button>
+    </td>`;
+  // alert(creatingEl.innerHTML);
+  if (name !== '' && num > 0) {
+    // alert(selectParent);
+    // alert(creatingEl);
+    selectParent.appendChild(creatingEl);
+    // alert(3);
+  }
+
+  selectParent.lastElementChild
+    .querySelector('.btn-remove')
+    .addEventListener('click', removeProduct);
+
+  nameInput.value = '';
+  numberInput.value = 0;
 }
 
 window.addEventListener('load', () => {
@@ -70,4 +108,7 @@ window.addEventListener('load', () => {
   removeButtons.forEach((item) =>
     item.addEventListener('click', removeProduct)
   );
+
+  const createProductBtn = document.querySelector('#create');
+  createProductBtn.addEventListener('click', createProduct);
 });
